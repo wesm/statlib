cdf342acd230f17ad8f1d23c48f55b1ba9ba34be
Wes McKinney <wesmckinn@gmail.com>
2011-02-21 22:14:02 -0500

    AR decomp refactoring

--- statlib/arma.py
+++ statlib/arma.py
@@ -21,9 +21,93 @@ class ARModel(object):
         self.data = data
         self.p = p
 
+        self.arx = data - data.mean()
+        self.exog, self.endog = _prep_arvars(self.arx, p)
+
+    @cache_readonly
+    def ref_results(self):
+        return sm.OLS(self.exog, self.endog).fit()
+
+    @property
+    def dlm_rep(self):
+        """
+        DLM form of AR(p) state transition matrix
+        """
+        beta = LA.lstsq(self.endog, self.exog)[0]
+        lower = np.c_[np.eye(self.p-1), np.zeros((self.p-1, 1))]
+        return np.vstack((beta, lower))
+
+    def decomp(self):
+        """
+        Compute decomposition of input time series into components
+
+        Notes
+        -----
+        W&H p. 302
+        Compute G = B A B^-1
+        Decompose x_t,j = (H theta_t)_j where H = B'E B^-1
+        E = (1, 0, ..., 0)
+
+        Returns
+        -------
+        tup : tuple (waves, mods, decomp)
+            wavelengths : length k
+            moduli : length k
+            decomp : n x k
+        """
+        p = self.p
+        X = self.endog
+
+        vals, vecs = LA.eig(self.dlm_rep)
+
+        H = np.dot(np.diag(vecs[0]), inv(vecs))
+
+        mods = np.abs(vals)
+        angles = np.angle(vals)
+        angles[angles == 0] = np.pi
+
+        # Put everything in order of increasing angles and ignore negative
+        # angles
+        sorter = np.argsort(angles)
+        angles = angles[sorter]
+        mask = angles > 0
+
+        angles = angles[mask]
+        mods = mods[sorter][mask]
+        vecs = vecs[sorter][mask]
+
+        # convert to real
+        H = H[sorter][mask].real
+        # double the complex H coefficients, z_t,j = x_t,d + x_t,h
+        # negative components are at the beginning
+        H[:(-mask).sum()] *= 2
+
+        states = np.c_[np.zeros((p, p-1)),   # "initial states"
+                       X.T,                  # regressor matrix
+                       self.arx[:-(p+1):-1]] # time t
+        decomp = np.dot(H, states).T
+
+        waves = 2 * np.pi / angles
+
+        return waves, mods, decomp
+
     def plot_acf(self, lags=50):
         plotting.plot_acf(self.data, lags)
 
+class TestARModel(object):
+
+    def test_decomp(self):
+        pass
+
+def _prep_arvars(x, p):
+    x = x - x.mean()
+    y = x[p:]
+    X = np.empty((p, len(y)))
+
+    for i in range(1, p + 1):
+        X[i-1] = x[p-i:-i]
+
+    return y, X.T
 
 class BayesLM(object):
     """
@@ -124,90 +208,36 @@ class BayesLM(object):
 
         return var_samples, np.array(beta_samples)
 
+def ma_coefs(coefs, maxn=10):
+    p = len(coefs)
+    phis = np.zeros(maxn+1)
+    # phis[0] = 1
+
+    # recursively compute MA coefficients
+
+    # for i in range(1, p):
+    #     for j in range(1, maxn):
+    #         phis[j] += coefs[i] * phis[j - i]
+
+    for i in xrange(1, maxn + 1):
+        for j in xrange(1, i+1):
+            if j > p:
+                break
+            phis[i] += phis[i-j] * coefs[j-1]
+
+    return phis
 
 class ARMA(object):
+    """
+    Estimation of ARMA(p, q) model via MCMC
+
+    Notes
+    -----
+    see Prado & West, pp. 75-77
+    cf. Marriott, Ravishanker, Gelfand, Pai (1996)
+    """
+
     pass
-
-def reference_ar(x, p):
-    y, X = _prep_arvars(x, p)
-    model = sm.OLS(y, X).fit()
-
-    # B = np.dot(X.T, X)
-
-    # beta = LA.solve(B, np.dot(X.T, y))
-    # resid = y - X.dot(beta)
-    # df = len(y) - p
-
-    # var = np.dot(resid, resid) / df
-
-    # return {
-    #     'beta' : beta,
-    #     'df' : df,
-    #     'var' : var,
-    #     'precision' : B,
-    #     'resid' : resid
-    # }
-
-    return model
-
-def _prep_arvars(x, p):
-    from scipy.linalg import hankel
-
-    # reverse order
-    demeaned = (x - x.mean())[::-1]
-
-    y = demeaned[:-p]
-    X = hankel(demeaned[1:-(p-1)], demeaned[-p:])
-
-    return y, X
-
-def ar_decomp(x, p, ncomp):
-    # Ported from Matlab
-
-    arx = x - x.mean()
-    y, X = _prep_arvars(x, p)
-    model = sm.OLS(y, X).fit()
-
-    lower = np.c_[np.eye(p-1), np.zeros((p-1, 1))]
-    G = np.vstack((model.params, lower))
-    vals, vecs = LA.eig(G)
-
-    # W&H p. 302
-    # Compute G = B A B^-1
-    # E = (1, 0, ..., 0)
-    # Decompose x_t,j = (H theta_t)_j
-    # where H = B'E B^-1
-    H = np.dot(np.diag(vecs[0]), inv(vecs))
-
-    mods = np.abs(vals)
-    angles = np.angle(vals)
-    angles[angles == 0] = np.pi
-
-    # Put everything in order of increasing angles and ignore negative angles
-    sorter = np.argsort(angles)
-    angles = angles[sorter]
-    mask = angles > 0
-
-    angles = angles[mask]
-    mods = mods[sorter][mask]
-    vecs = vecs[sorter][mask]
-
-    # convert to real
-    H = H[sorter][mask].real
-
-    # double the complex H coefficients, z_t,j = x_t,d + x_t,h
-    # negative components are at the beginning
-    H[:(-mask).sum()] *= 2
-
-    # TODO: rewrite this in a less convoluted manner
-    decomp = np.dot(H, np.c_[arx[:-(p+1):-1], X.T, np.zeros((p, p-1))])
-
-    # X is in reverse order
-    decomp = np.fliplr(decomp).T
-
-    waves = 2 * np.pi / angles
-
-    return waves, mods, decomp
 
 def ar_simulate(phi, s, n, dist='normal'):
     """
@@ -233,7 +263,7 @@ if __name__ == '__main__':
     # model = sm_arma(eeg)
     # res = model.fit(order=(8, 0), method='css', disp=-1)
 
-    model = ARModel(eeg)
+    model = ARModel(eeg, p=8)
 
     p = 8
     y, X = _prep_arvars(eeg, p)
@@ -241,4 +271,4 @@ if __name__ == '__main__':
     beta_prior = (np.zeros(p), np.eye(p) * 100)
     var_prior = (2, 1000)
 
-    model = BayesLM(y, X, beta_prior, var_prior)
+    model2 = BayesLM(y, X, beta_prior, var_prior)
